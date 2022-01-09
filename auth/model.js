a3283722b0c83ba00ebd2108ee297e962fdbe92c
ercumentlacin <ercument.lacin@akinon.com>
2022-01-09 09:00:09 +0300

    Add trim for username and password

--- auth/model.js
+++ auth/model.js
@@ -7,15 +7,17 @@ const userSchema = new mongoose.Schema({
     unique: true,
     minlength: 2,
     maxlength: 30,
+    trim: true,
     validate: {
       validator: (value) => /^[a-zA-Z0-9]+$/.test(value),
-      message: '{VALUE} is not a valid username!',
+      message: `{VALUE} is not a valid username!`,
     },
   },
   password: {
     type: String,
     required: true,
     minlength: 6,
+    trim: true,
   },
   createdAt: { type: Date, default: Date.now },
 });
