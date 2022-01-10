94024006d2e07ad6faadf186fc2bfdb6a27354b6
ercumentlacin <ercument.lacin@akinon.com>
2022-01-10 13:36:17 +0300

    Cors

--- index.js
+++ index.js
@@ -20,6 +20,24 @@ mongoose.connect(process.env.MONGODB_URI, {
   (err) => errorHandler(err),
 );
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World!',
